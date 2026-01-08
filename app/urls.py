3925e8636a34a73e0660e6a4aeb8415fb6f93ffc
Cyopn <2021452176@teschi.edu.mx>
2026-01-08 05:26:12 -0600

    fix(new views)

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,8 @@ from app.login.login_view import LoginView
 from app.register.register_view import RegisterView
 from app.index.index_view import IndexView
 from app.employee.employee_view import EmployeeView
+from app.entry.entry_view import EntryView
+from app.allocation.allocation_view import AllocationView
 
 urlpatterns = [
     path('', RootView.as_view(), name='home'),
@@ -11,4 +13,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('index/', IndexView.as_view(), name='index'),
     path('employee/', EmployeeView.as_view(), name='employee'),
+    path('entry/', EntryView.as_view(), name='entry'),
+    path('allocation/', AllocationView.as_view(), name='allocation'),
 ]
